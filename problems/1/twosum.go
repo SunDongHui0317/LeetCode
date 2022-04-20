0e99c309f6bf6b95b7bd1e786f5fbe3dac540824
Sun Donghui <donghui.sun@rdc-west.com>
2022-04-20 20:58:17 +0800

    feat: add leet code problems 1

---
+++ problems/1/twosum.go
@@ -0,0 +1,19 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	hash := make(map[int]int)
+	for i, item := range nums {
+		hash[item] = i+1
+	}
+	for i, item := range nums {
+		v := target - item
+		if hash[v] > 0 && (hash[v] -1) != i{
+			return []int{
+				i,
+				hash[v]-1,
+			}
+		}
+	}
+	return []int{}
+}
+
---
+++ problems/1/twosum_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	nums := []int{
+		2,7,11,15,
+	}
+	target := 22
+	expected := []int{
+		1,3,
+	}
+	assert.Equal(t, twoSum(nums, target), expected)
+}
